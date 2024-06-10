b1d9d8d9bef7a29fc93b11fe7edacf75d671c698
GustavodeLima0 <127445817+GustavodeLima0@users.noreply.github.com>
2024-06-10 14:59:49 -0300

    Add files via upload

---
+++ pet-dogui-poo/src/main/java/br/unipar/pet/dogui/poo/PetDoguiPoo.java
@@ -0,0 +1,61 @@
+package br.unipar.pet.dogui.poo;
+
+import br.unipar.pet.dogui.poo.domain.Cachorro;
+import br.unipar.pet.dogui.poo.domain.Cor;
+import br.unipar.pet.dogui.poo.domain.Pelagem;
+import br.unipar.pet.dogui.poo.domain.Raca;
+import br.unipar.pet.dogui.poo.exceptions.NegocioException;
+import br.unipar.pet.dogui.poo.services.CachorroService;
+import br.unipar.pet.dogui.poo.services.CorService;
+import br.unipar.pet.dogui.poo.services.PelagemService;
+import br.unipar.pet.dogui.poo.services.RacaService;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class PetDoguiPoo {
+
+    public static void main(String[] args) {
+        try {
+            CorService corService = new CorService();
+            PelagemService pelagemService = new PelagemService();
+            RacaService racaService = new RacaService();
+            CachorroService cachorroService = new CachorroService();
+
+
+            Cor cor = new Cor(0, "Marrom");
+            Pelagem pelagem = new Pelagem(0, "Curta");
+            Raca raca;
+            raca = new Raca(0, "Labrador");
+
+            cor = corService.insert(cor);
+            pelagem = pelagemService.insert(pelagem);
+            raca = racaService.insert(raca);
+
+            Cachorro cachorro = new Cachorro(0, "Rex", raca, pelagem, cor, 50.0, true, (java.sql.Date) new Date());
+            cachorro = cachorroService.insert(cachorro);
+            
+            Cachorro cachorro1 = new Cachorro(0, "Max", raca, pelagem, cor, 50.0, true, (java.sql.Date) new Date());
+            cachorro1 = cachorroService.insert(cachorro1);
+            System.out.println("Cores:");
+            for (Cor c : corService.findAll()) {
+                System.out.println(c);
+            }
+
+            System.out.println("Pelagens:");
+            for (Pelagem p : pelagemService.findAll()) {
+                System.out.println(p);
+            }
+
+            System.out.println("Raças:");
+            for (Raca r : racaService.findAll()) {
+                System.out.println(r);
+            }
+
+            System.out.println("Cachorros:");
+            for (Cachorro c : cachorroService.findAll()) {
+                System.out.println(c);
+            }
+        } catch (SQLException | NegocioException ex) {
+        }
+    }
+}
---
+++ pet-dogui-poo/src/main/java/br/unipar/pet/dogui/poo/services/CachorroService.java
@@ -0,0 +1,69 @@
+package br.unipar.pet.dogui.poo.services;
+
+
+import br.unipar.pet.dogui.poo.domain.Cachorro;
+import br.unipar.pet.dogui.poo.exceptions.NegocioException;
+import br.unipar.pet.dogui.poo.respositories.CachorroRepository;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CachorroService {
+
+    public Cachorro insert(Cachorro cachorro) throws SQLException, NegocioException {
+        validate(cachorro);
+        CachorroRepository cachorroRepository = new CachorroRepository();
+        return cachorroRepository.insert(cachorro);
+    }
+
+    public Cachorro update(Cachorro cachorro) throws SQLException, NegocioException {
+        validate(cachorro);
+        validateUpdate(cachorro);
+        CachorroRepository cachorroRepository = new CachorroRepository();
+        return cachorroRepository.update(cachorro);
+    }
+
+    public void delete(int id) throws SQLException {
+        CachorroRepository cachorroRepository = new CachorroRepository();
+        cachorroRepository.delete(id);
+    }
+
+    public Cachorro findById(int id) throws SQLException {
+        CachorroRepository cachorroRepository = new CachorroRepository();
+        return cachorroRepository.findById(id);
+    }
+
+    public ArrayList<Cachorro> findAll() throws SQLException {
+        CachorroRepository cachorroRepository = new CachorroRepository();
+        return cachorroRepository.findAll();
+    }
+
+    private void validate(Cachorro cachorro) throws NegocioException {
+        if (cachorro.getNome() == null || cachorro.getNome().isBlank()) {
+            throw new NegocioException("O nome do Cachorro deve ser informado.");
+        }
+        if (cachorro.getNome().length() > 60) {
+            throw new NegocioException("O nome do Cachorro não deve possuir mais do que 60 caracteres.");
+        }
+        if (cachorro.getTamanho() == null || cachorro.getTamanho() <= 0) {
+            throw new NegocioException("O tamanho do Cachorro deve ser informado e deve ser maior que zero.");
+        }
+        if (cachorro.getDtNascimento() == null) {
+            throw new NegocioException("A data de nascimento do Cachorro deve ser informada.");
+        }
+        if (cachorro.getRaca() == null || cachorro.getRaca().getId() == 0) {
+            throw new NegocioException("A raça do Cachorro deve ser informada.");
+        }
+        if (cachorro.getPelagem() == null || cachorro.getPelagem().getId() == 0) {
+            throw new NegocioException("A pelagem do Cachorro deve ser informada.");
+        }
+        if (cachorro.getCor() == null || cachorro.getCor().getId() == 0) {
+            throw new NegocioException("A cor do Cachorro deve ser informada.");
+        }
+    }
+
+    private void validateUpdate(Cachorro cachorro) throws NegocioException {
+        if (cachorro.getId() == 0) {
+            throw new NegocioException("Informe um código válido para atualização do cachorro.");
+        }
+    }
+}

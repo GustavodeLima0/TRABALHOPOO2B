85ae75dc47b63743cca49d0d411c49121685c34f
GustavodeLima0 <127445817+GustavodeLima0@users.noreply.github.com>
2024-06-11 15:08:57 -0300

    Delete pet-dogui-poo directory

--- pet-dogui-poo/src/main/java/br/unipar/pet/dogui/poo/exceptions/NegocioException.java
+++
@@ -1,9 +0,0 @@
-package br.unipar.pet.dogui.poo.exceptions;
-
-public class NegocioException extends Exception {
-    
-    public NegocioException(String mensagem){
-        super(mensagem);
-    }
-    
-}
